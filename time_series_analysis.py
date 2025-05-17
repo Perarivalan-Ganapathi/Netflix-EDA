ba4797e007bff945637b5394ec00c22e5fd03025
Perarivalan Ganapathi <96115177+Perarivalan-Ganapathi@users.noreply.github.com>
2025-05-17 17:39:38 +0530

    Update time_series_analysis.py

--- time_series_analysis.py
+++ time_series_analysis.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sna
 
-df=pd.read_csv("C:\\Users\\perar\\Desktop\\Netflix_movies_and_tv_shows_clustering.csv\\Cleaned_Netflix_Data.csv")
+df=pd.read_csv("Cleaned_Netflix_Data.csv")
 
 df['date_added'] = pd.to_datetime(df['date_added'])
 
